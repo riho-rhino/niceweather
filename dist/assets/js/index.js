9220a4a5a146fb17160f0c4d0cf0ce69ee9b8e84
Riho yamamoto <r.yamamoto@rhino-inc.jp>
2025-04-10 12:40:31 +0900

    04.10

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,9 +1,6 @@
-(()=>{"use strict";var e={"./src/assets/js/_modules/_test.js":
-/*!*****************************************!*\
-  !*** ./src/assets/js/_modules/_test.js ***!
-  \*****************************************/(e,t,o)=>{o.r(t),o.d(t,{default:()=>r});const r=function(){console.log("hellow 01")}}},t={};function o(r){var s=t[r];if(void 0!==s)return s.exports;var l=t[r]={exports:{}};return e[r](l,l.exports,o),l.exports}o.d=(e,t)=>{for(var r in t)o.o(t,r)&&!o.o(e,r)&&Object.defineProperty(e,r,{enumerable:!0,get:t[r]})},o.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),o.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})};var r={};
+(()=>{"use strict";var e={r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}};
 /*!********************************!*\
   !*** ./src/assets/js/index.js ***!
   \********************************/
-o.r(r),(0,o(/*! ./_modules/_test */"./src/assets/js/_modules/_test.js").default)()})();
+e.r({}),Object(function(){var e=new Error("Cannot find module './_modules/_test'");throw e.code="MODULE_NOT_FOUND",e}()),Object(function(){var e=new Error("Cannot find module './_modules/_test'");throw e.code="MODULE_NOT_FOUND",e}())()})();
 //# sourceMappingURL=index.js.map
